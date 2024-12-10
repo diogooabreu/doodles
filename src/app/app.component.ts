43ea5464349d9fac972f4eb2257ce0252b47d821
Diogo Abreu <abreudiogo177@gmail.com>
2024-12-10 08:26:14 -0300

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadComponent } from "./head/head.component";
 import { NavComponent } from "./nav/nav.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadComponent, NavComponent],
+  imports: [RouterOutlet, HeadComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
